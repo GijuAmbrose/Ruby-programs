6308e865095674065c1758545fd5cbae3bca9b94
Giju Ambrose <jijoakalarikkal@gmail.com>
2019-08-06 15:46:36 +0530

    Perimeter and area

--- AreaOfCircle.rb
+++ AreaOfCircle.rb
@@ -1,7 +1,8 @@
 class AreaOfCircle
     
     def initialize(input)
-        puts "The area of circle is:#{input*input*3.14}   "    
+        puts "The area of circle is:#{input*input*3.14}   " 
+        puts "The perimiter of circle is: #{2*input*3.14}"    
     end
 
 end
